3db022f4a054c1ab9ab01b92fdc85d90852b062a
Siri <lakshmisiri2003@gmail.com>
2024-03-13 20:14:17 +0530

    SecondCommit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "This is Siri, SRN: PES2UG21CS911\n";
+  cout << "This is Siri, SRN: PES2UG21CS911, Section: 6H\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
